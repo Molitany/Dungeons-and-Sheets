7fe1d5f9f0271e215a9867f43c7d5d54928fa081
Thor Beregaard <thor@beregaard.dk>
2022-09-12 19:02:19 +0200

    rework stucture to support better building and deployment

---
+++ Dungeons and Sheets/Program.cs
@@ -0,0 +1,39 @@
+using backend.Controller.User;
+using backend.Database;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews().AddNewtonsoftJson();
+
+builder.Services.AddEndpointsApiExplorer();
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 30));
+builder.Services.AddDbContext<DnD5eContext>(options => options.UseMySql(builder.Configuration.GetConnectionString("string"), serverVersion));
+builder.Configuration.AddJsonFile("appsettings.json");
+builder.Services.AddTransient<UserService>();
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "api/{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();
