ba1e41161a493579c9fa4c05a3a2147cd0490565
Malthemester <malthemester@gmail.com>
2022-09-12 13:07:59 +0200

    Start woring on Template and CharacterBuilder

---
+++ frontend/src/pages/CharacterBuilder/CharacterBuilder.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+function CharacterBuilder() {
+
+    return (
+        <div>
+            THIS Builds
+
+        </div>
+    )
+}
+
+export default CharacterBuilder;
--- frontend/src/pages/LandingPage/Landing.tsx
+++ frontend/src/pages/LandingPage/Landing.tsx
@@ -1,13 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from "axios"
 import Button from "react-bootstrap/Button"
+import Form from 'react-bootstrap/Form';
 import useAuth from '../../AuthProvider/Auth';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+
 
 function Landing() {
-  const {logout} = useAuth()
+  const { logout } = useAuth()
+
+  const [template, setTemplate] = useState('DND');
+
+  function HandelSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+    console.log(template)
+    let navigate = useNavigate()
+    navigate("/CharacterBuilder")
+
+  }
+
   return (
     <div>
       THIS WORKSx
+
+      <Form onSubmit={HandelSubmit}>
+        <Row>
+          <Col lg="3">
+            <InputGroup>
+              <Form.Select onChange={(e) => setTemplate(e.target.value)}>
+                <option disabled hidden value="">Choose you template</option>
+                <option value="DND">D&d 5e</option>
+                <option value="FATE">Fate</option>
+              </Form.Select>
+              <Button variant="primary" type="submit"> New Charactar </Button>
+            </InputGroup>
+          </Col>
+        </Row>
+      </Form>
+
       <Button onClick={() => logout()}>Click me for logout</Button>
     </div>
   )
